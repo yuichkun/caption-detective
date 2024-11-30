0f722cec92a7d0b6ef394117ec1fc486079aab71
yuichkun <yuichkun@gmail.com>
2024-12-01 06:36:35 +0900

    add content script

---
+++ content.ts
@@ -0,0 +1,8 @@
+import type { PlasmoCSConfig } from "plasmo"
+ 
+export const config: PlasmoCSConfig = {
+  matches: ["https://www.youtube.com/"],
+  all_frames: true
+}
+
+console.log("🚀Caption Enhancer loaded!")
