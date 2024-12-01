9d8e8a18c149e158d9057c024aae737b973d0b1f
yuichkun <yuichkun@gmail.com>
2024-12-01 19:16:26 +0900

    update proper noun extractor

--- lib/properNounExtractor.ts
+++ lib/properNounExtractor.ts
@@ -1,3 +1,5 @@
+import { DomainDetector } from "./domainDetector"
+
 const systemPrompt = `Extract proper nouns from the text.
 Output only proper nouns separated by commas.
 
@@ -46,5 +48,98 @@ export async function extractProperNouns(
     }
   }
 
-  return properNouns
+  const uniqueProperNouns = [...new Set(properNouns)]
+  console.log("uniqueProperNouns", uniqueProperNouns)
+  const verifiedProperNouns = await verifyProperNoun(
+    uniqueProperNouns.join(", ")
+  )
+  console.log("verifiedProperNouns", verifiedProperNouns)
+  const suspiciousNounList = await checkSuspiciousNoun(
+    verifiedProperNouns.join(", ")
+  )
+  console.log("suspiciousNounList", suspiciousNounList)
+
+  const domain = await new DomainDetector().detect(description)
+  console.log("domain", domain)
+  if (!domain) return suspiciousNounList
+  const relatedProperNounList = await Promise.all(
+    suspiciousNounList.map((noun) => brainStormRelatedNoun(noun, domain))
+  )
+  const properNounList = [
+    ...suspiciousNounList,
+    ...relatedProperNounList.flat()
+  ]
+    .map((noun) => noun.replace('"', ""))
+    .map((noun) => noun.replace("'", ""))
+    .map((noun) => noun.replace("`", ""))
+    .map((noun) => noun.trim())
+  const uniqueProperNounList = [...new Set(properNounList)]
+  return uniqueProperNounList
+}
+
+const verifyProperNounPrompt = `Your task is to filter out the non-proper nouns from the input list.
+Output only the proper nouns separated by commas.
+
+Example:
+Input: "cat, https://www.youtube.com, 12345, Microsoft Excel"
+Output: "Microsoft Excel"`
+async function verifyProperNoun(nounList: string) {
+  const session = await window.ai.languageModel.create({
+    systemPrompt: verifyProperNounPrompt
+  })
+  const response = await session.prompt(`Input: "${nounList}"`)
+  const verifiedNounList = response
+    .split(",")
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0)
+  return [...new Set(verifiedNounList)]
+}
+
+const checkSuspiciousNounPrompt = `Your task is to pick out the nouns that may not be transcribed correctly from the input list.
+Output only the nouns that may not be transcribed correctly separated by commas.
+
+Example:
+Input: "Vue, Apple, London, Michael Jackson"
+Output: "Vue"`
+async function checkSuspiciousNoun(nounList: string) {
+  const session = await window.ai.languageModel.create({
+    systemPrompt: checkSuspiciousNounPrompt
+  })
+  const response = await session.prompt(`Input: "${nounList}"`)
+  const suspiciousNounList = response
+    .split(",")
+    .map((item) => item.trim())
+    .filter((item) => item.length > 0)
+  return [...new Set(suspiciousNounList)]
+}
+
+const brainStormRelatedNounPrompt = `Your task is to generate related proper nouns in the same input domain as the input noun.
+
+Output only the related proper nouns separated by commas.
+
+Example:
+Input domain: "art"
+Input proper noun: "Basquiat"
+Output: "Picasso, Andy Warhol"
+
+Input domain: "technology"
+Input proper noun: "Apple"
+Output: "iPhone, MacBook, iMac"`
+async function brainStormRelatedNoun(noun: string, domain: string) {
+  const session = await window.ai.languageModel.create({
+    systemPrompt: brainStormRelatedNounPrompt
+  })
+  try {
+    const response = await session.prompt(
+      `Input domain: "${domain}"\nInput proper noun: "${noun}"`
+    )
+    const relatedNounList = response
+      .split(",")
+      .map((item) => item.trim())
+      .filter((item) => item.length > 0)
+    return [...new Set(relatedNounList)]
+  } catch (e) {
+    console.error(e)
+    return []
+  }
 }
